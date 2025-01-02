1a899819b5b98313aa7785798a0917662d4bc735
parkaward <parksangjun1363@gmail.com>
2025-01-02 13:29:28 +0900

    chore: init Electorn Descktop App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {Button} from './components/ui/button'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Click me</Button>
-
+      <Button >Click me</Button>
     </>
   )
 }
